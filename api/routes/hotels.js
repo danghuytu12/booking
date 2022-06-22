d7db116f4b56eebfae3e6be7003bfb9626ea9091
Huy Tú <danghuytu.thd2006@gmail.com>
2022-06-22 23:03:30 +0700

    post, put hotel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -14,5 +14,14 @@ router.post("/", async (req, res) => {
         res.status(500).json(error);
     }
 })
+// update
+router.put("/:id", async (req, res) => {
+    try {
+        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true});
+        res.status(200).json(updateHotel);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 
 export default router;
